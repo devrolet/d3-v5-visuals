cdfd767fbeb6edf7278527020bcf53ebcf265c8a
Chet Hill <chetdhill@gmail.com>
2023-11-08 15:51:54 -0600

    Initial Commit: D3 Env Setup

---
+++ D3/code/script.js
@@ -0,0 +1,24 @@
+let language = "english"
+let electionType = "Type 2";
+
+switch(electionType) {
+  case "Type 1":
+    console.log("This is Type 1");
+    break;
+  case "Type 2":
+    language === "english" ? "Hello" : "Hola";
+    console.log(language);
+    break;
+  case "Type 3":
+    console.log("This is Type 3");
+    break;
+  case "Type 4":
+    console.log("This is Type 4");
+    break;
+  case "Type 5":
+    console.log("This is Type 5");
+    break;
+  default:
+    console.log("Hello World");
+}
+
